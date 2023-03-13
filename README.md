d76272d45dd5c5436d1ad4d8d5937050f6994918
root <root@chatGPT.lan>
2023-03-13 12:19:50 +0000

    add promps

---
+++ README.md
@@ -0,0 +1,3 @@
+A demo repo based on OpenAI GPT-3.5 Turbo API.
+
+yarn dev --host 0.0.0.0 --port 666
---
+++ src/default.ts
@@ -0,0 +1,11 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 填入自己的 key 才可使用。
+- **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
